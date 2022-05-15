064495becf718e881cc1a0db12385563548b3dca
SamiS7 <sami-abbas-ali@gmx.net>
2022-05-15 20:45:21 +0200

    update order.html

--- js/order.js
+++ js/order.js
@@ -1,31 +1,29 @@
-let json = localStorage.getItem('shoppingCart');
-json = JSON.parse(json);
-let sum = 0;
+$(() => {
+    let json = localStorage.getItem('shoppingCart');
+    json = JSON.parse(json);
+    let sum = 0;
 
-$('#shoppingCart > #products').html('');
-for (let p of json.products) {
-    const { img, name, amount, price } = p;
-    $('#shoppingCart > #products').append(`
-    <div class="product">
-        <img src="${img}">
-        <div class="textBox">
-            <h3 class="productName" title="${name}">${name}</h5>
-            <div>
-            <p>${amount}x</p>
-            <p class="priceP">${p.price}€</p>
+    //$('#productBox').html('');
+    for (let p of json.products) {
+        const { img, name, amount, price } = p;
+        $('#productBox').append(`
+        <div class="itemBox">
+            <div class="imageBox">
+                <img src="${img}" alt="${name}">
+                <div class="textBox">
+                    <p>${amount}x ${name}</p>
+                    <p style="margin-left:300px;">${price}</p>
+                </div>
             </div>
         </div>
-        <div class="borderBTW"></div>
-        <div class="modificationBox">
-            <div>
-                <button class="minusB">-</button>
-                <p class="productNumberP">${amount}</p>
-                <button class="plusB">+</button>
-            </div>
-            <img class="deleteB" src="../img/trash-solid.svg">
-        </div>
-    </div>
     `);
 
-    sum += amount * parseInt(price);
-}
+        sum += amount * parseInt(price);
+    }
+    $('#productBox').append(`
+        <div id="priceBox">
+            <p>Gesamt</p>
+            <p id="shoppingCartPrice">${sum}€</p>
+        </div>
+    `);
+});
