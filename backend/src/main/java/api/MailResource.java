31c85ec593b3e528bafb6884322bef1b39f34f40
David <dp.precup@gmail.com>
2022-05-31 08:08:11 +0200

    nur package change

---
+++ backend/src/main/java/api/MailResource.java
@@ -0,0 +1,42 @@
+package api;
+
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import io.quarkus.mailer.reactive.ReactiveMailer;
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/mail")
+public class MailResource {
+
+    @Inject Mailer mailer;
+
+    @GET
+    @Blocking
+    public void sendEmail() {
+        mailer.send(
+                Mail.withText("dp.precup@gmail.com",
+                        "Ahoy from Quarkus",
+                        "A simple email sent from a Quarkus application."
+                )
+        );
+    }
+
+    @Inject
+    ReactiveMailer reactiveMailer;
+
+    @GET
+    @Path("/reactive")
+    public Uni<Void> sendEmailUsingReactiveMailer() {
+        return reactiveMailer.send(
+                Mail.withText("dp.precup@gmail.com",
+                        "Ahoy from Quarkus",
+                        "A simple email sent from a Quarkus application using the reactive API."
+                )
+        );
+    }
+}
