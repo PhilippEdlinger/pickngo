ccde6f457ab50edf3a6bb0d79cd9b20eec907360
it180171 <david.altenhofer@gmail.com>
2022-06-01 11:34:04 +0200

    update html & css

---
+++ frontend/src/app/components/order-end/order-end.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-end',
+  templateUrl: './order-end.component.html',
+  styleUrls: ['./order-end.component.scss']
+})
+export class OrderEndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
